c55d619ba21b1d68b76bcaedeb61d424f7152515
Rizky Ramadhan <ramagiox@gmail.com>
2017-10-11 14:26:54 +0700

    perubahan rama
    let surattugasRoute = require('./surattugas/surattugasRoute.js');
    app.use('/api',surattugasRoute);
    
    let tipepegawaiRoute = require('./tipepegawai/tipepegawaiRoute.js');
    app.use('/api',tipepegawaiRoute);
    
    let pegawaiRoute = require('./pegawai/pegawaiRoute.js');
    app.use('/api',pegawaiRoute);
    
    let surattugasrinciRoute = require('./surattugasrinci/surattugasrinciRoute.js');
    app.use('/api',surattugasrinciRoute);

--- pelatihan/index.js
+++ pelatihan/index.js
@@ -26,6 +26,20 @@ app.use('/api',penilaianTeknisRoute);
 let penilaianAkhirRoute = require('./penilaianAkhir/penilaianAkhirRoute.js');
 app.use('/api',penilaianAkhirRoute);
 
+//rama
+
+let surattugasRoute = require('./surattugas/surattugasRoute.js');
+app.use('/api',surattugasRoute);
+
+let tipepegawaiRoute = require('./tipepegawai/tipepegawaiRoute.js');
+app.use('/api',tipepegawaiRoute);
+
+let pegawaiRoute = require('./pegawai/pegawaiRoute.js');
+app.use('/api',pegawaiRoute);
+
+let surattugasrinciRoute = require('./surattugasrinci/surattugasrinciRoute.js');
+app.use('/api',surattugasrinciRoute);
+
 // let pembayaranRoute = require('./pembayaran/pembayaranRoute.js');
 // app.use('/api',pembayaranRoute);
 
